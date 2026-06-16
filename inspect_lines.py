bc2b82316565a816e023a9c4cadc11719056868f
sanalsaji00-hub <sanalsaji00@gmail.com>
2026-06-16 15:21:09 +0530

    16-06-2026

--- inspect_lines.py
+++
@@ -1,8 +0,0 @@
-
-path = r"d:/Projects/Testa-website/index.html"
-with open(path, "rb") as f:
-    lines = f.readlines()
-    print(lines[840]) # 0-indexed, so line 841 in 1-indexed.
-    print(lines[840].decode('utf-8', errors='replace'))
-    print(lines[845]) # line 846
-    print(lines[845].decode('utf-8', errors='replace'))
